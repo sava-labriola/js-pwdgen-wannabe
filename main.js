70af6f47dec82e3ff585bc219e014d9ab1017cc6
sava-labriola <labriolasava00@gmail.com>
2020-04-17 15:40:41 +0200

    javascript funzionante

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ var nome = prompt('Inserisci il tuo nome');
 var cognome = prompt('Inserisci il tuo cognome');
 //Chiede all'utente il suo colore preferito
 var colore = prompt('Inserisci il tuo colore preferito');
+//Recupera data corrente
+var data = new Date();
+//Recupera anno e converte in stringa
+var anno = data.getFullYear().toString();
+//Recupera solo ultimi caratteri
+var anno_cut = anno.substr(2, 3);
 //Unisce i dati inseriti in una variabile
-var password = (nome + cognome + colore + '20')
+var password_generata = (nome + cognome + colore + anno_cut);
 //Inserisce la password generata in h1
-document.getElementById('password').innerHTML = password;
+document.getElementById('password').innerHTML = password_generata;
